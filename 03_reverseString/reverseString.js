6a84820f3d6a7c1817f2fd1b8527a2e93a863509
arkyvio <arkyvio@gmail.com>
2022-05-14 11:15:17 -0400

    Function Test Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(userInput) {
+    let array = userInput.split('');
+    let revArray = array.reverse();
+    let endString = revArray.join('');
+    return endString;
 };
 
 // Do not edit below this line
