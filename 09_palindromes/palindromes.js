0e042f6ae0e902f75559e81739b32f01d678b5f3
arkyvio <arkyvio@gmail.com>
2022-05-14 19:40:28 -0400

    Function Test Completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (userStr) {
+
+   let strNoWhiteSpace = userStr.replace(/[^a-z0-9]/gi, '');
+   let strNoUpperCase = strNoWhiteSpace.toLowerCase();
+   let userArr = strNoUpperCase.split('');
+   let checkArr = userArr.slice().reverse();
+   let isPalindrome = false;
+
+   for (let i = 0; i < userArr.length; i++) {
+      if (isPalindrome === true || i === 0) {
+         if (userArr[i] === checkArr[i]) {
+            isPalindrome = true;
+         } else {
+            isPalindrome = false;
+         }
+      }
+   }
+
+   return isPalindrome;
 
 };
 
