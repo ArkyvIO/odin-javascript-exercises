ff453d36856b8dd3f4eb44dad3d7369fcce2b387
arkyvio <arkyvio@gmail.com>
2022-05-14 12:39:09 -0400

    Function Test Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  let converted = (f - 32) * (5 / 9)
+  return Math.round(converted * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  let converted = (c * (9 / 5)) + 32
+  return Math.round(converted * 10) / 10;
 };
 
 // Do not edit below this line
