0c9fc9c478ffa3247d77204e02f2fda3e13f354f
arkyvio <arkyvio@gmail.com>
2022-05-14 11:51:49 -0400

    Function Test Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let sum = 0;
+    let x;
 
+    // Swap if larger number first
+    if (start > end) {
+        x = start;
+        start = end;
+        end = x;
+    }
+
+    if (typeof start === 'number' && typeof end === 'number' && start >= 0 && end >= 0) {
+        if (start < end) {
+            for (i = start; i <= end; i++)
+                sum += i;
+        };
+    } else {
+        return 'ERROR';
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
