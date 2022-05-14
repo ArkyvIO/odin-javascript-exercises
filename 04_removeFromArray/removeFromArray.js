e87af4ef0f76239c315868e5cd2d19f43cc56809
arkyvio <arkyvio@gmail.com>
2022-05-14 11:35:21 -0400

    Function Test Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(userArr, ...args) {
+    return userArr.filter(i => !args.includes(i));
 };
 
 // Do not edit below this line
