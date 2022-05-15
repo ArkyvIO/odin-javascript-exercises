b5f8af4941aaecee869caf89cb0ec3ce76de698b
arkyvio <arkyvio@gmail.com>
2022-05-15 17:13:36 -0400

    Function Test Completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (x) {
+    x.sort((a, b) => {
+        let currentYear = new Date().getFullYear();
+        let xa;
+        let xb;
+        
+        if ('yearOfDeath' in a) {
+            xa = (a.yearOfDeath - a.yearOfBirth);
+        } else {
+            xa = (currentYear - a.yearOfBirth);
+        }
+  
+        if ('yearOfDeath' in b) {
+            xb = (b.yearOfDeath - b.yearOfBirth);
+        } else {
+            xb = (currentYear - b.yearOfBirth);
+        }
 
+        return xb - xa;
+    })
+    return x[0];
 };
 
 // Do not edit below this line
